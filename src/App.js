5ef3823e5ada8e73b5747b63ccd8a0d406129402
johnathantng <johnathan.tng@gmail.com>
2019-07-18 19:33:03 -0700

    finished lesson 25

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +6,15 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asda'
-        },
-        {
-          name: 'Dracula',
-          id: 'awda'
-        },
-        {
-          name: 'Zombie',
-          id: 'adwa'
-        }
-      ]
+      monsters: []
     }
   };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
 
   render() {
     return (
