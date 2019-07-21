5215a5bf1d8afa64e31b763219cd52cf93273fbb
johnathantng <johnathan.tng@gmail.com>
2019-07-21 01:36:29 -0700

    most recent changes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,5 +6,6 @@ export const Card = (props) => (
 	<div className="card-container">
 		<img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
 		<h2> {props.monster.name} </h2>
+		<p> {props.monster.email} </p>
 	</div>
 );
